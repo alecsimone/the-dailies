a60b94639b64cee0f4e8f5ad2fc3bb5af312e9c8
alecsimone <alecsimone@gmail.com>
2020-02-01 23:35:54 -0700

    BottomBar input

--- Frontend/components/BottomBar.js
+++ Frontend/components/BottomBar.js
@@ -1,14 +1,42 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import { setAlpha, setLightness } from '../styles/functions';
 
 const StyledBottomBar = styled.section`
    width: 100%;
-   background: ${props => setLightness(props.theme.black, 1)};
    border-top: 2px solid ${props => setAlpha(props.theme.lowContrastGrey, 0.25)};
    display: flex;
+   position: relative;
    justify-content: stretch;
    ${props => props.theme.mobileBreakpoint} {
       display: none;
+   }
+   .inputWrapper {
+      width: 100%;
+      position: absolute;
+      height: 6rem;
+      max-height: 6rem;
+      top: calc(-6rem - 2px);
+      left: 0;
+      padding: 0 1rem;
+      background: ${props => props.theme.black};
+      transition: all 0.1s;
+      z-index: 2;
+      &.hidden {
+         max-height: 0;
+         top: 0;
+         overflow: hidden;
+      }
+      input {
+         width: 100%;
+         margin-top: 1.25rem;
+         padding: 0 2rem;
+         height: 3.5rem;
+         background: ${props => setAlpha(props.theme.lowContrastGrey, 0.2)};
+         border-radius: 0;
+         font-size: ${props => props.theme.bigText};
+         z-index: 2;
+      }
    }
    .bottomBarButton {
       padding: 1.75rem 0;
@@ -16,10 +44,12 @@ const StyledBottomBar = styled.section`
       text-align: center;
       font-weight: 700;
       font-size: ${props => props.theme.bigText};
+      background: ${props => setLightness(props.theme.black, 1)};
       line-height: 1;
       cursor: pointer;
       border-right: 2px solid
          ${props => setAlpha(props.theme.lowContrastGrey, 0.25)};
+      z-index: 3;
       &:last-child {
          border-right: none;
       }
@@ -30,12 +60,37 @@ const StyledBottomBar = styled.section`
 `;
 
 const BottomBar = () => {
-   const a = '0';
+   const [inputPlaceholder, setInputPlaceholder] = useState('');
    return (
       <StyledBottomBar>
-         <div className="bottomBarButton">S</div>
+         <div
+            className={`inputWrapper${
+               inputPlaceholder == false ? ' hidden' : ''
+            }`}
+         >
+            <input type="text" placeholder={inputPlaceholder} />
+         </div>
+         <div
+            className="bottomBarButton"
+            onClick={() => {
+               setInputPlaceholder(
+                  inputPlaceholder === 'Search' ? false : 'Search'
+               );
+            }}
+         >
+            S
+         </div>
          <div className="bottomBarButton">H</div>
-         <div className="bottomBarButton">+</div>
+         <div
+            className="bottomBarButton"
+            onClick={() => {
+               setInputPlaceholder(
+                  inputPlaceholder === 'Add Post' ? false : 'Add Post'
+               );
+            }}
+         >
+            +
+         </div>
       </StyledBottomBar>
    );
 };
