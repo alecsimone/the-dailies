f9f40a2ac123c60181e3539af318619f05cfce4c
alecsimone <alecsimone@gmail.com>
2020-02-01 18:36:24 -0700

    More rich text formatting in stylishtext

--- Frontend/components/StylishText.js
+++ Frontend/components/StylishText.js
@@ -1,37 +1,66 @@
 import PropTypes from 'prop-types';
 
 const StylishText = ({ text }) => {
-   if (text == null || typeof text !== 'string' || text === '') {
+   if (
+      text == null ||
+      typeof text !== 'string' ||
+      text === '' ||
+      !process.browser
+   ) {
       return text;
    }
-   const searchString = /<style="(.+)">(.+)<\/style>/gi;
-   const simpleTagSearch = text.match(searchString);
+   const styleTagSearchString = /(?:(?<style><style="(?<styleObjectRaw>.+)">(?<styleTextContent>.+)<\/style>)|(?<stars>\*\*(?<starsTextContent>.+)\*\*)|(?<bars>__(?<barsTextContent>.+)__))/gi;
+   // const styleTagRegexp = new RegExp(
+   //    '<style="(?<named>.+)">(.+)<\\/style>',
+   //    'gi'
+   // );
+   // const x = text.matchAll(styleTagRegexp);
+   // console.log(x);
+   const simpleTagSearch = text.match(styleTagSearchString);
    if (simpleTagSearch == null) {
       return <>{text}</>;
    }
-   const tags = text.matchAll(searchString);
+   const tags = text.matchAll(styleTagSearchString);
    const elementsArray = [];
    let stoppedAtIndex = 0;
    for (const tag of tags) {
       const startingText = text.substring(stoppedAtIndex, tag.index);
       elementsArray.push(startingText);
 
-      const splitTag = tag[1].split(/[:;]/gi);
-      const styleObject = {};
-      splitTag.forEach((tagPiece, index) => {
-         if (index % 2 === 1) {
-            return;
-         }
-         styleObject[splitTag[index].trim()] = splitTag[index + 1].trim();
-      });
-      // styleObject = { [splitTag[0]]: splitTag[1].trim() };
+      if (tag.groups.style != null) {
+         const splitTag = tag.groups.styleObjectRaw.split(/[:;]/gi);
+         const styleObject = {};
+         splitTag.forEach((tagPiece, index) => {
+            if (index % 2 === 1) {
+               return;
+            }
+            styleObject[splitTag[index].trim()] = splitTag[index + 1].trim();
+         });
+         // styleObject = { [splitTag[0]]: splitTag[1].trim() };
 
-      const tagElement = (
-         <span style={styleObject} key={stoppedAtIndex}>
-            {tag[2]}
-         </span>
-      );
-      elementsArray.push(tagElement);
+         const tagElement = (
+            <span style={styleObject} key={stoppedAtIndex}>
+               {tag.groups.styleTextContent}
+            </span>
+         );
+         elementsArray.push(tagElement);
+      }
+
+      if (tag.groups.stars != null) {
+         elementsArray.push(
+            <span style={{ fontWeight: 700, color: 'white' }}>
+               {tag.groups.starsTextContent}
+            </span>
+         );
+      }
+
+      if (tag.groups.bars != null) {
+         elementsArray.push(
+            <span style={{ fontStyle: 'italic' }}>
+               {tag.groups.barsTextContent}
+            </span>
+         );
+      }
 
       stoppedAtIndex = tag.index + tag[0].length;
    }
