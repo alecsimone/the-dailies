edc0342c063c1cd8eb923f5283738fddb2c3de28
alecsimone <alecsimone@gmail.com>
2020-11-12 21:56:45 -0700

    update vh on resize

--- Frontend/components/Page.js
+++ Frontend/components/Page.js
@@ -583,6 +583,11 @@ const Page = ({ children, pageProps }) => {
 
       const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+      window.addEventListener('resize', () => {
+         const vh = window.innerHeight * 0.01;
+         document.documentElement.style.setProperty('--vh', `${vh}px`);
+      });
    }, []);
 
    return (
