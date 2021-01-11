3380402f3dd15bb947de8694310dac6c53dc40f0
alecsimone <alecsimone@gmail.com>
2021-01-11 12:27:18 -0700

    Querying for member data again

--- Frontend/components/Account/MemberProvider.js
+++ Frontend/components/Account/MemberProvider.js
@@ -24,50 +24,50 @@ const ME_SUBSCRIPTION = gql`
 const MemberContext = React.createContext();
 
 const MemberProvider = ({ children, isHome }) => {
-   // const { loading, error, data, client } = useQuery(CURRENT_MEMBER_QUERY);
+   const { loading, error, data, client } = useQuery(CURRENT_MEMBER_QUERY);
 
-   // // const {
-   // //    data: subscriptionData,
-   // //    loading: subscriptionLoading
-   // // } = useSubscription(ME_SUBSCRIPTION);
+   // const {
+   //    data: subscriptionData,
+   //    loading: subscriptionLoading
+   // } = useSubscription(ME_SUBSCRIPTION);
 
-   // let memberData = {};
-   // if (error) {
-   //    console.log(error);
-   //    memberData.me = 'error';
-   // }
+   let memberData = {};
+   if (error) {
+      console.log(error);
+      memberData.me = 'error';
+   }
 
-   // if (data != null) {
-   //    if (data.me == null) {
-   //       memberData.me = null;
-   //    } else {
-   //       memberData.me = data.me;
-   //    }
-   // } else {
-   //    memberData = {
-   //       loading
-   //    };
-   // }
-
-   const memberData = {
-      me: {
-         __typename: 'Member',
-         id: 'ck68ae27l0vmf079555gghsue',
-         displayName: 'Alec',
-         avatar:
-            'https://pbs.twimg.com/profile_images/917202644740956160/lMFbGZ-e_400x400.jpg',
-         rep: 10,
-         friends: [],
-         broadcastView: false,
-         notifications: [],
-         friendRequests: [],
-         ignoredFriendRequests: [],
-         defaultExpansion: true,
-         defaultPrivacy: 'Friends',
-         role: 'Member',
-         twitterUserName: 'alecsimone'
+   if (data != null) {
+      if (data.me == null) {
+         memberData.me = null;
+      } else {
+         memberData.me = data.me;
       }
-   };
+   } else {
+      memberData = {
+         loading
+      };
+   }
+
+   // const memberData = {
+   //    me: {
+   //       __typename: 'Member',
+   //       id: 'ck68ae27l0vmf079555gghsue',
+   //       displayName: 'Alec',
+   //       avatar:
+   //          'https://pbs.twimg.com/profile_images/917202644740956160/lMFbGZ-e_400x400.jpg',
+   //       rep: 10,
+   //       friends: [],
+   //       broadcastView: false,
+   //       notifications: [],
+   //       friendRequests: [],
+   //       ignoredFriendRequests: [],
+   //       defaultExpansion: true,
+   //       defaultPrivacy: 'Friends',
+   //       role: 'Member',
+   //       twitterUserName: 'alecsimone'
+   //    }
+   // };
 
    return (
       <MemberContext.Provider value={memberData}>
