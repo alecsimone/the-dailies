9370f8838a160cff90444709771ae7380fdec9e3
alecsimone <alecsimone@gmail.com>
2021-01-11 14:02:08 -0700

    push summary instead of unshifting it

--- Frontend/components/ThingCards/ThingCard.js
+++ Frontend/components/ThingCards/ThingCard.js
@@ -259,6 +259,7 @@ const ThingCard = ({ data, setExpanded, borderSide }) => {
    }
    const contentArray = orderContent(fullContent, contentOrder);
    if (summary != null && summary !== '' && !contentArray.includes(summary)) {
+      contentArray.push(summary);
       // contentArray.unshift(summary);
    }
 
