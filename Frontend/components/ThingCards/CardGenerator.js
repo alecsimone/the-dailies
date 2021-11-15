2f3669032b1577e1797ce3d202e8ac94ade1717a
alecsimone <alecsimone@gmail.com>
2021-11-15 14:40:39 -0700

    CardGenerator checks the store first

--- Frontend/components/ThingCards/CardGenerator.js
+++ Frontend/components/ThingCards/CardGenerator.js
@@ -1,49 +1,32 @@
-import { useQuery } from '@apollo/react-hooks';
 import PropTypes from 'prop-types';
-import { SINGLE_THING_QUERY } from '../../pages/thing';
+import { useSelector } from 'react-redux';
 import FlexibleThingCard from './FlexibleThingCard';
-import Error from '../ErrorMessage';
-import LoadingRing from '../LoadingRing';
-import useMe from '../Account/useMe';
-import useQueryAndStoreIt from '../../stuffStore/useQueryAndStoreIt';
+import CardFetcher from './CardFetcher';
 
 const CardGenerator = ({ id, cardType, borderSide }) => {
-   const { data, loading, error } = useQueryAndStoreIt(SINGLE_THING_QUERY, {
-      variables: {
-         id
-      },
-      fetchPolicy: 'cache-first'
-   });
+   const hasData = useSelector(state => state.stuff[`Thing:${id}`] != null);
 
-   const {
-      loggedInUserID,
-      memberFields: { role }
-   } = useMe('CardGenerator', 'role');
-
-   if (data) {
-      if (data.thing == null) {
-         return <Error error={{ message: `No thing found for id: ${id}` }} />;
-      }
+   if (!hasData) {
       return (
-         <FlexibleThingCard
-            key={data.thing.id}
-            expanded={cardType === 'regular'}
-            thingID={data.thing.id}
-            contentType={cardType === 'regular' ? 'full' : 'single'}
-            canEdit={
-               loggedInUserID != null &&
-               (data.thing.author.id === loggedInUserID ||
-                  ['Admin', 'Editor', 'Moderator'].includes(role))
-            }
-            titleLink
+         <CardFetcher
+            thingID={id}
+            cardType={cardType}
             borderSide={borderSide}
          />
       );
    }
-   if (error) {
-      return <Error error={error} />;
-   }
-   return <LoadingRing />;
+
+   return (
+      <FlexibleThingCard
+         key={id}
+         expanded={cardType === 'regular'}
+         thingID={id}
+         contentType={cardType === 'regular' ? 'full' : 'single'}
+         canEdit={false}
+         titleLink
+         borderSide={borderSide}
+      />
+   );
 };
 CardGenerator.propTypes = {
    id: PropTypes.string.isRequired,
