45c664f6514342aeedd437c4d98e4bbff2b3b00f
alecsimone <alecsimone@gmail.com>
2021-12-05 17:15:27 -0700

    no rich text test

--- Frontend/components/RichText.js
+++ Frontend/components/RichText.js
@@ -18,6 +18,7 @@ import {
 import { urlFinder } from '../lib/UrlHandling';
 
 const RichText = ({ text, priorText, nextText, matchCount = 0 }) => {
+   return text;
    let fixedText = replaceReddit(replaceEmails(replaceTwitterMentions(text)));
 
    if (
