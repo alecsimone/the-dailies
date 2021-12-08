162a799bb75f1b9b2473ca9c0ac0d12ee11a5ecf
alecsimone <alecsimone@gmail.com>
2021-12-08 13:42:26 -0700

    New attempt at favicons

--- Frontend/components/Header/Meta.js
+++ Frontend/components/Header/Meta.js
@@ -5,10 +5,34 @@ const Meta = () => (
    <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      {/* <link rel="icon" href="/favicon.ico" sizes="any" />
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="/site.webmanifest" /> */}
+
+      <link
+         rel="apple-touch-icon"
+         sizes="180x180"
+         href="/apple-touch-icon.png?v=2"
+      />
+      <link
+         rel="icon"
+         type="image/png"
+         sizes="32x32"
+         href="/favicon-32x32.png?v=2"
+      />
+      <link
+         rel="icon"
+         type="image/png"
+         sizes="16x16"
+         href="/favicon-16x16.png?v=2"
+      />
+      <link rel="manifest" href="/site.webmanifest?v=2" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#0066cc" />
+      <link rel="shortcut icon" href="/favicon.ico?v=2" />
+      <meta name="msapplication-TileColor" content="#030303" />
+      <meta name="theme-color" content="#0066cc" />
+
       <link rel="stylesheet" type="text/css" href="/nprogress.css" />
       <link rel="stylesheet" href="https://use.typekit.net/iwq0uru.css" />
       <title>Ouryou</title>
